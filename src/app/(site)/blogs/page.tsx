511167dbda2ce30d704f9f5032c57dae925be5c7
megha <megha.girisagar@ishafoundation.org>
2024-11-09 23:40:18 +0530

    blog page updates

--- src/app/(site)/blogs/page.tsx
+++ src/app/(site)/blogs/page.tsx
@@ -33,4 +33,3 @@ export default async function BlogPage() {
   );
 }
 
-export default BlogPage;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -118,7 +118,7 @@ const Footer = () => {
                 <ul>
                   <li>
                     <Link
-                      href="/resources/blog"
+                      href="/blog"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       Blog
