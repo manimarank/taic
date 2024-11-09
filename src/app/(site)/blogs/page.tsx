14288b2698b4fdf5182a56986bb18ee91eae5f81
manimarank <manimarank@users.noreply.github.com>
2024-11-09 22:34:45 +0530

    Update page.tsx

--- src/app/(site)/blogs/page.tsx
+++ src/app/(site)/blogs/page.tsx
@@ -10,12 +10,7 @@ export const metadata: Metadata = {
 };
 
 export default async function BlogPage() {
-  let posts = [];
-  try {
-    posts = await getPosts();
-  } catch (error) {
-    console.error("Failed to fetch posts:", error);
-  }
+  const posts = await getPosts();
 
   return (
     <>
@@ -27,13 +22,10 @@ export default async function BlogPage() {
       <section className="pb-[120px] pt-[120px]">
         <div className="container">
           <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
-            {posts.length > 0 ? (
+            {posts.length > 0 &&
               posts.map((blog, index) => (
                 <SingleBlog key={index} blog={blog} />
-              ))
-            ) : (
-              <p>No posts available at the moment.</p>
-            )}
+              ))}
           </div>
         </div>
       </section>
